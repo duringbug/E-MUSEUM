042f2f3aa10a816448aee79e274061e8b94af5ba
duringbug <2034298621@qq.com>
2023-02-21 13:14:52 +0800

    "chatgpt"

--- src/view/Comment.vue
+++ src/view/Comment.vue
@@ -17,14 +17,6 @@ import io from 'socket.io-client'
 /**
  * chatapi
  */
-// const api = new ChatGPTAPI({
-//     apiKey: process.env.OPENAI_API_KEY,
-//     debug: true
-// })
-// const res = await api.sendMessage('what is the answer to the universe?', {
-//     promptPrefix: `You are ChatGPT, a large language model trained by OpenAI. You answer as concisely as possible for each response (e.g. don’t be verbose). It is very important that you answer as concisely as possible, so please remember this. If you are generating a list, do not have too many items. Keep the number of items short.
-// Current date: ${new Date().toISOString()}\n\n`
-// })
 /**
  * websocket
  */
